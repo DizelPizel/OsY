9c428dc6239627378c4aa3a1bfe5dab2b417cb94
DizelPizel <98840046+DizelPizel@users.noreply.github.com>
2022-04-12 16:13:33 +0300

    Create shmem.h

---
+++ shmem.h
@@ -0,0 +1,13 @@
+#ifndef INC_4_LAB_SHRMEM_H
+#define INC_4_LAB_SHRMEM_H
+
+#include <fcntl.h>
+
+const size_t map_size = 4096;
+
+const char * BackingFile1 = "os_lab4.1.back";
+const char * SemaphoreName1 = "os_lab4.1.semaphore";
+
+unsigned AccessPerms = S_IWUSR | S_IRUSR | S_IRGRP | S_IROTH;
+
+#endif //INC_4_LAB_SHRMEM_H
