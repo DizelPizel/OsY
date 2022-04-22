9256eb928ea3296f6fe02d3cb33a83b8cd222e3f
DizelPizel <98840046+DizelPizel@users.noreply.github.com>
2022-04-23 00:44:58 +0300

    Update child.cpp

--- laba 4/child.cpp
+++ laba 4/child.cpp
@@ -13,15 +13,13 @@ int main(void)
     std::string tmp;
     int i = 0;
     int map_fd;
-    sem_t *semptr;
-    map_fd = shm_open(BackingFile1, O_RDWR, AccessPerms);
-    semptr = sem_open(SemaphoreName1, O_CREAT, AccessPerms, 2);
+    map_fd = shm_open(BackingFile1, O_RDWR, AccessPerms); // открытие общей области памяти
     if (map_fd < 0)
     {
         perror("shm_open");
         exit(EXIT_FAILURE);
     }
-    caddr_t memptr = mmap(
+    caddr_t memptr = mmap( // проецируем память к себе
         NULL,
         map_size,
         PROT_READ | PROT_WRITE,
@@ -33,48 +31,7 @@ int main(void)
         perror("mmap");
         exit(EXIT_FAILURE);
     }
-    if (semptr == SEM_FAILED)
-    {
-        perror("semptr");
-        exit(EXIT_FAILURE);
-    }
-
-    if (sem_wait(semptr) != 0)
-    {
-        perror("sem_wait");
-        exit(EXIT_FAILURE);
-    }
-    int val;
-    while (memptr[0] != EOF)
-    {
-        if (sem_getvalue(semptr, &val) != 0)
-        {
-            perror("sem_getvalue");
-            exit(EXIT_FAILURE);
-        }
-        if (val == 2)
-        {
-            continue;
-        }
-        if (sem_wait(semptr) != 0)
-        {
-            perror("sem_wait");
-            exit(EXIT_FAILURE);
-        }
-        if (memptr[0] == EOF)
-        {
-            break;
-        }
-        if (memptr[0] == '\0')
-        {
-            if (sem_post(semptr) != 0)
-            {
-                perror("sem_post");
-                exit(EXIT_FAILURE);
-            }
-            continue;
-        }
-        while (std::getline(std::cin, tmp))
+        while (std::getline(std::cin, tmp)) 
         {
             float delimoe;
             int delitel;
@@ -89,14 +46,7 @@ int main(void)
                 }
                 delimoe /= delitel;
             }
-            if (sem_post(semptr) != 0)
-            {
-                perror("sem_post");
-                exit(EXIT_FAILURE);
-            }
-            printf("%s", delimoe);
-            sprintf(memptr, "%s", delimoe);
+            sprintf(memptr, "%s", delimoe); // печатаем в область памяти
         }
-    }
     return 0;
 }
